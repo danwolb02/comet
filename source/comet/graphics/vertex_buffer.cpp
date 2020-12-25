c1763f177f87babdda4ebfeadec686316c6cffa3
Daniel Wolbach <danwolb02@gmail.com>
2020-12-25 21:46:10 +0100

    Add vertex buffer

---
+++ source/comet/graphics/vertex_buffer.cpp
@@ -0,0 +1,50 @@
+#define COMET_IMPLEMENTATION
+
+#include <comet/external.hpp>
+#include <comet/graphics/vertex_buffer.hpp>
+
+namespace comet
+{
+VertexBuffer::VertexBuffer(const Vec<f32> &data)
+{
+    GL_CALL(glGenBuffers(1, &handle));
+    if (!handle) throw RuntimeError("Unable to create vertex buffer.");
+
+    bind();
+    GL_CALL(glBufferData(42, data.size() * sizeof(f32), data.data(), GL_STATIC_DRAW));
+}
+
+VertexBuffer::VertexBuffer(VertexBuffer &&other)
+: handle(other.handle)
+{
+    other.handle = 0;
+}
+
+VertexBuffer::~VertexBuffer()
+{
+    release();
+}
+
+auto VertexBuffer::operator=(VertexBuffer &&other) -> VertexBuffer &
+{
+    if (this != &other)
+    {
+        release();
+        handle = other.handle;
+        other.handle = 0;
+    }
+
+    return *this;
+}
+
+auto VertexBuffer::release() -> void
+{
+    if (handle) glDeleteBuffers(1, &handle);
+}
+
+auto VertexBuffer::bind() const -> void
+{
+    GL_CALL(glBindBuffer(GL_ARRAY_BUFFER, handle));
+}
+
+}
--- test/sandbox/source/main.cpp
+++ test/sandbox/source/main.cpp
@@ -1,10 +1,12 @@
 #include <comet/graphics/window.hpp>
+#include <comet/graphics/vertex_buffer.hpp>
 
 using namespace comet;
 
 int main()
 {
     Window window(1280, 720, "Comet Application");
+    VertexBuffer vertex_buffer({34, 44, 66});
 
     while (!window.should_close())
     {
