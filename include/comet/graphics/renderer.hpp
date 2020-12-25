76858af9f1ab33cfe5b4c5e51183b6a7e092c100
Daniel Wolbach <danwolb02@gmail.com>
2020-12-25 23:57:54 +0100

    Add shader

--- include/comet/graphics/renderer.hpp
+++ include/comet/graphics/renderer.hpp
@@ -1,17 +1,18 @@
 #pragma once
 
 #include <comet/graphics/vertex_array.hpp>
+#include <comet/graphics/shader.hpp>
 
 namespace comet
 {
 class Renderer
 {
 public:
-    Renderer() = default;
+    Renderer();
 
     ~Renderer() = default;
 
-    auto render(const VertexArray &vertex_array) -> void;
+    auto render(const VertexArray &vertex_array, const Shader &shader) -> void;
 };
 
 }
